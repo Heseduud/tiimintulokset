cb2159a538e22b4c9de9216215b96d59cde05252
Santeri Nurminen <nusanteri@gmail.com>
2021-03-06 16:57:26 +0200

    Refactor matchistory endpoint to use axios

--- backend/controllers/players.js
+++ backend/controllers/players.js
@@ -3,7 +3,6 @@
  */
 
 const playerRouter = require('express').Router();
-const request = require('request');
 const axios = require('axios');
 const _ = require('lodash');
 
@@ -15,34 +14,30 @@ playerRouter.get('/', async (_req, res) => {
   res.json(players);
 });
 
-// test player id: 788da801-a3da-4cc6-87db-acbb612e9bb7
-// test url: localhost:3001/api/players/matchHistory/788da801-a3da-4cc6-87db-acbb612e9bb7
 playerRouter.get('/matchHistory/:id', async (req, res) => {
   const MATCHHISTORYURL = `${config.FACEIT_MATCHHISTORY_URL}/${req.params.id}/history?game=csgo&offset=0&limit=20`;
 
-  // TODO: refactor this to use axios, no idea why i initially used request
-  request.get({ ...config.FACEIT_API_AUTH, url: MATCHHISTORYURL }, async (err, response, body) => {
-    if (err) {
-      console.error(err);
+  const playerData = await axios.get(MATCHHISTORYURL, {
+    headers: {
+      Authorization: config.FACEIT_API_TOKEN
     }
-
-    const data = JSON.parse(body);
-    const parsedHistory = [];
-    _.forEach(data.items, (match) => {
-      const resObj = {
-        id: match.match_id,
-        finished: match.finished_at,
-        teams: {
-          faction1: match.teams.faction1.nickname,
-          faction2: match.teams.faction2.nickname
-        }, 
-        winner: match.results.winner
-      };
-      parsedHistory.push(resObj);
-    });
-
-    res.json(parsedHistory);
   });
+
+  const parsedHistory = [];
+  _.forEach(playerData.data.items, (match) => {
+    const resObj = {
+      id: match.match_id,
+      finished: match.finished_at,
+      teams: {
+        faction1: match.teams.faction1.nickname,
+        faction2: match.teams.faction2.nickname
+      },
+      winner: match.results.winner
+    };
+    parsedHistory.push(resObj);
+  });
+
+  res.json(parsedHistory);
 });
 
 playerRouter.get('/info/:username', async (req, res) => {
